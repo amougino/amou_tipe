46b377fccda86108b18a503cd421363be9d538c8
Arthur Mouginot <mouginot.arthur@gmail.com>
2026-04-03 21:21:45 +0200

    init3

---
+++ thren/test.py
@@ -0,0 +1,12 @@
+import thren_v_alpha as thren
+
+
+tau = (0, 500000)
+prec = 10000
+
+settings = thren.get_single_settings()
+a = thren.calculate(settings, tau)
+
+thren.plot_traj(a, settings, tau, prec)
+
+thren.fig.savefig("fronde_deux_corps_amouginot.pdf")
