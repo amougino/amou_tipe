d251d92b96ca0c2707b9f720cf20bf44e8ca4485
Arthur Mouginot <mouginot.arthur@gmail.com>
2026-05-21 16:20:20 +0200

    cleanup

--- thren/e_sat.py
+++ thren/e_sat.py
@@ -4,37 +4,37 @@ w.i.p.
 
 '''
 
-import thren_v_2 as thren
+import thren.sim as sim
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-settings = thren.get_single_settings()
+settings = sim.get_single_settings()
 
 n_of_points = 5000
 
-end_time = thren.end_time_approx(settings)
+end_time = sim.end_time_approx(settings)
 timespan = (0, end_time)
 time_values = np.linspace(0, end_time, n_of_points)
 
-a = thren.calculate(settings, timespan, time_values=time_values, method="DOP853")
+a = sim.calculate(settings, timespan, time_values=time_values, method="DOP853")
 
 x_sat = np.array(a.y[0])
 y_sat = np.array(a.y[1])
 vx_sat = np.array(a.y[2])
 vy_sat = np.array(a.y[3])
-v_sat = thren.pyth(vx_sat, vy_sat)
+v_sat = sim.pyth(vx_sat, vy_sat)
 
 t = np.array(a.t)
 
-R1x, R1y, R2x, R2y = thren.body_positions(settings, t)
+R1x, R1y, R2x, R2y = sim.body_positions(settings, t)
 
-d1 = thren.pyth(x_sat - R1x, y_sat - R1y)
-d2 = thren.pyth(x_sat - R2x, y_sat - R2y)
+d1 = sim.pyth(x_sat - R1x, y_sat - R1y)
+d2 = sim.pyth(x_sat - R2x, y_sat - R2y)
 
 print(min(d1), min(d2))
 
-e = (0.5*(v_sat**2)) - ((thren.G*settings["mass1"])/d1) - ((thren.G*settings["mass2"])/d2)
+e = (0.5*(v_sat**2)) - ((sim.G*settings["mass1"])/d1) - ((sim.G*settings["mass2"])/d2)
 
 fig, ax = plt.subplots()
 ax.plot(t, e)
--- thren/test.py
+++ thren/test.py
@@ -1,19 +1,19 @@
-import thren_v_2 as thren
+import thren.sim as sim
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 
-settings = thren.get_single_settings()
+settings = sim.get_single_settings()
 
 n_of_points = 1000
 
-end_time = thren.end_time_approx(settings)
+end_time = sim.end_time_approx(settings)
 timespan = (0, end_time)
 time_values = np.linspace(0, end_time, n_of_points)
 
-a = thren.calculate(settings, timespan, time_values=time_values)
+a = sim.calculate(settings, timespan, time_values=time_values)
 
 # thren.plot_traj(a, settings, timespan)
 
-thren.animate_traj(a, settings, abs(settings["sat_pos"]["y"]), time_values, simulation_time=5, trail_fraction=10)
+sim.animate_traj(a, settings, abs(settings["sat_pos"]["y"]), time_values, simulation_time=5, trail_fraction=10)
